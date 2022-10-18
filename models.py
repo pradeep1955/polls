721fb0c2ecc37e32c7b78e4461f585f103129ff8
pradeep1955 <pradeep_chowdhary@hotmail.com>
2022-10-18 09:36:07 +0530

    polls app 18-10-22

---
+++ models.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from time import timezone
+from django.db import models
+
+class Question(models.Model):
+  question_text = models.CharField(max_length = 200)
+  pub_date = models.DateTimeField('Date of publication')
+  
+
+  def __str__(self):
+    return self.question_text
+    
+  def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)    
+
+
+class Choice(models.Model):
+  question = models.ForeignKey(Question, on_delete= models.CASCADE)
+  choice_text = models.CharField(max_length = 200)
+  vote = models.IntegerField(default = 0)
+
+  def __str__(self):
+    return self.choice_text
+
+
